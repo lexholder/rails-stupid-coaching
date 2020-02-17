49622c2bc95a56341e2fd3db55ece0494080c507
Alex Holder <lexholder@gmail.com>
2020-02-17 14:19:55 -0500

    updated style

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,10 +5,20 @@ class QuestionsController < ApplicationController
   def answer
     if params[:question].end_with?('?')
       @question = 'Silly question, get dressed and go to work!'
+      @yourquestion = params[:question]
+      @told = 'You asked the coach:'
+      @coach = 'The coach replied:'
     elsif params[:question] == 'I am going to work'
-      @great = 'Great!'
+      @great = 'Good job!'
+      @yourquestion = params[:question]
+      @told = 'You told the coach what he wanted to hear:'
+      @coach = 'The coach replied:'
     else
       @care = "I don't care, get dressed and go to work!"
+      @yourquestion = params[:question]
+      @told = 'You told the coach:'
+      @coach = 'The coach replied:'
+      @conversation = 'Dialogue'
     end
   end
 end
